de853192b70d82ececd703910efc92f1e50a0b75
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-19 16:37:02 -0300

    fix: fixed imports and comments

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import datetime
 import os
 from github import Github
 from github import Auth
 
+# Retrieving input variables from environment variables
 github_token = os.environ['INPUT_TOKEN']
 repo = os.environ['INPUT_REPOSITORY']
 developer_team_slug = os.environ['INPUT_DEVELOPER_TEAM_SLUG']
@@ -12,6 +14,7 @@ print(repo)
 print(developer_team_slug)
 print(admin_team_slug)
 
+# Creating authentication object with the provided token
 auth = Auth.Token(github_token)
 
 # Initialize the GitHub API client
@@ -20,26 +23,23 @@ github_instance = Github(auth=auth)
 # Get the repository
 repo = github_instance.get_repo(repo)
 
+# Calculate the timestamps for 2 and 3 weeks ago
+two_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=2)
+three_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=3)
+
+# Identify and manage branches
 for branch in repo.get_branches():
-    print(branch.name)
+    if branch.name == repo.default_branch:
+        # Skip the default branch
+        continue
 
-# # Calculate the timestamps for 2 and 3 weeks ago
-# two_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=2)
-# three_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=3)
+    last_commit = branch.commit.commit.author.date
+    last_activity = branch.commit.commit.committer.date if branch.commit.commit.committer else last_commit
 
-# # Identify and manage branches
-# for branch in repo.get_branches():
-#     if branch.name == repo.default_branch:
-#         # Skip the default branch
-#         continue
+    if last_commit < two_weeks_ago:
+        # Mark as stale (more than 2 weeks old)
+        branch.edit(protection=False)
 
-#     last_commit = branch.commit.commit.author.date
-#     last_activity = branch.commit.commit.committer.date if branch.commit.commit.committer else last_commit
-
-#     if last_commit < two_weeks_ago:
-#         # Mark as stale (more than 2 weeks old)
-#         branch.edit(protection=False)
-
-#     if last_activity < three_weeks_ago and not branch.is_merged():
-#         # Prune if not merged and more than 3 weeks old with no new activity
-#         branch.delete()
+    if last_activity < three_weeks_ago and not branch.is_merged():
+        # Prune if not merged and more than 3 weeks old with no new activity
+        branch.delete()
