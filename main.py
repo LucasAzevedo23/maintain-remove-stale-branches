d7716b90c37a8e5e8252ab72f152810e9cdc0132
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-19 15:57:05 -0300

    test: testing accessing input values from environment variables

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from github import Github
 from github import Auth
 
-# Initialize the GitHub API client
-g = Github(github_token)
+github_token = os.environ['INPUT_TOKEN']
+repo = os.environ['INPUT_REPOSITORY']
+developer_team_slug = os.environ['INPUT_DEVELOPER_TEAM_SLUG']
+admin_team_slug = os.environ['INPUT_ADMIN_TEAM_SLUG']
 
-# Get the repository
-repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
+print(github_token)
+print(repo)
+print(developer_team_slug)
+print(admin_team_slug)
 
-# Calculate the timestamps for 2 and 3 weeks ago
-two_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=2)
-three_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=3)
+# # Initialize the GitHub API client
+# github_instance = Github(os.environ['GITHUB_TOKEN'])
 
-# Identify and manage branches
-for branch in repo.get_branches():
-    if branch.name == repo.default_branch:
-        # Skip the default branch
-        continue
+# # Get the repository
+# repo = github_instance.get_repo(os.environ['GITHUB_REPOSITORY'])
 
-    last_commit = branch.commit.commit.author.date
-    last_activity = branch.commit.commit.committer.date if branch.commit.commit.committer else last_commit
+# # Calculate the timestamps for 2 and 3 weeks ago
+# two_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=2)
+# three_weeks_ago = datetime.datetime.now() - datetime.timedelta(weeks=3)
 
-    if last_commit < two_weeks_ago:
-        # Mark as stale (more than 2 weeks old)
-        branch.edit(protection=False)
+# # Identify and manage branches
+# for branch in repo.get_branches():
+#     if branch.name == repo.default_branch:
+#         # Skip the default branch
+#         continue
 
-    if last_activity < three_weeks_ago and not branch.is_merged():
-        # Prune if not merged and more than 3 weeks old with no new activity
-        branch.delete()
+#     last_commit = branch.commit.commit.author.date
+#     last_activity = branch.commit.commit.committer.date if branch.commit.commit.committer else last_commit
+
+#     if last_commit < two_weeks_ago:
+#         # Mark as stale (more than 2 weeks old)
+#         branch.edit(protection=False)
+
+#     if last_activity < three_weeks_ago and not branch.is_merged():
+#         # Prune if not merged and more than 3 weeks old with no new activity
+#         branch.delete()
