39679034b50ae3b1faba1c25c887aa2cde726f18
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-20 15:30:13 -0300

    feat: test file, changes on main and workflow example

--- main.py
+++ main.py
@@ -35,11 +35,3 @@ for branch in repo.get_branches():
 
     last_commit = branch.commit.commit.author.date
     last_activity = branch.commit.commit.committer.date if branch.commit.commit.committer else last_commit
-
-    if last_commit < two_weeks_ago:
-        # Mark as stale (more than 2 weeks old)
-        branch.edit(protection=False)
-
-    if last_activity < three_weeks_ago and not branch.is_merged():
-        # Prune if not merged and more than 3 weeks old with no new activity
-        branch.delete()
---
+++ test.py
@@ -0,0 +1,91 @@
+import datetime
+from github import Github
+from github import Auth
+
+# Inputs
+token="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"
+repo_slug="wexinc/ps-ds-peng-docs"
+admin_team_slug="platform-eng-admin"
+developer_team_slug="platform-eng"
+
+# Structures to be used
+dict_branches = []
+array_admins = []
+array_developers = []
+array_merged_branches = []
+
+# Github Library initial configuration
+auth = Auth.Token(token)
+github_instance = Github(auth=auth)
+
+# Repo and Org retrieval
+repo = github_instance.get_repo(repo_slug)
+org = github_instance.get_organization("wexinc")
+
+# Branches, teams and closed pull requests retrieval
+branches = repo.get_branches()
+pull_requests = repo.get_pulls(state='closed')
+teams = org.get_teams()
+
+for pr in pull_requests:
+    array_merged_branches.append(pr.head.ref)
+
+print("-- Merged branches --")
+print(array_merged_branches)
+print()
+
+for team in teams:
+    # Append admin members to array_admins
+    if team.name == admin_team_slug:
+        for member in team.get_members():
+            user = github_instance.get_user(member.login)
+            array_admins.append({'name': user.name, 'email': user.email, 'login': user.login})
+    # Append developer members to array_developers
+    if team.name == developer_team_slug:
+        for member in team.get_members():
+            user = github_instance.get_user(member.login)
+            array_developers.append({'name': user.name, 'email': user.email, 'login': user.login})
+
+print("-- Admins --")
+print(array_admins)
+print()
+
+print("-- Developers --")
+print(array_developers)
+print()
+
+for branch in branches:
+    # Check if branch is main or dev and skip
+    if branch.name == "main" or branch.name == "dev":
+        continue
+
+    # Setup date variables and delta between now and last modified
+    date_now = datetime.datetime.utcnow()
+    date_last_modified = branch.commit.commit.last_modified
+    converted = datetime.datetime.strptime(date_last_modified, '%a, %d %b %Y %H:%M:%S GMT')
+    delta = date_now - converted
+
+
+    # Check if branch author is not in users array
+    # if branch.commit.commit.author.email not in array_admins and branch.commit.commit.author.email not in array_developers:
+    #     dict_branches.append({ 'branch_name': branch.name,
+    #             'author_name': branch.commit.commit.author.name,
+    #             'author_email': branch.commit.commit.author.email,
+    #             'last_modified': delta.days,
+    #             'should_be_deleted': True })
+    # Check if branch author is older than 14 days
+    if delta.days >= 14:
+        dict_branches.append({ 'branch_name': branch.name,
+                'author_name': branch.commit.commit.author.name,
+                'author_email': branch.commit.commit.author.email,
+                'last_modified': delta.days,
+                'should_be_deleted': True })
+    else:
+        dict_branches.append({ 'branch_name': branch.name,
+                'author_name': branch.commit.commit.author.name,
+                'author_email': branch.commit.commit.author.email,
+                'last_modified': delta.days,
+                'should_be_deleted': False })
+
+print()
+print(dict_branches)
